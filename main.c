43dacfbdeb71c86f9dcfc02d48ba3392eb609f1c
lesyeuxdef <decepticonsrcool@gmail.com>
2024-10-06 10:00:54 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ctest
+//
+//  Created by kay kay kay on 10/6/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
