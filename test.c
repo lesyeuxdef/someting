778a2eb17eb95c33c12dc6fba65ca9f69bd44d6d
lesyeuxdef <decepticonsrcool@gmail.com>
2024-10-06 10:55:49 +0800

    gvg

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int candyA,candyB,candyC;
+
+    char choice;
+
+    printf("what candy would you like\n");
+    while(1) {
+        choice=getchar();
+    }
+}
